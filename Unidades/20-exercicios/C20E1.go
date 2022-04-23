7da0f2cc15f42ca6d78b568c9eac09102c77e848
Mr. Erlison <sisal.cordame_0a@icloud.com>
2022-04-23 16:53:01 -0300

    Update added exercises numbers

--- Unidades/20-exercicios/C20E1.go
+++ Unidades/20-exercicios/C20E1.go
@@ -1,3 +1,4 @@
+// C20E1
 // - Alem da goroutine principal, crie duas outras goroutines.
 // - Cada goroutine adicional devem fazer um print separado.
 // - Utilize waitgroups para fazer com que suas goroutines finalizem antes de o programa terminar.
